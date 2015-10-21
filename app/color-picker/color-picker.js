ef49745d6b1de560a60779388c3e45a29ddc7e19
azahreba <azahreba@gmail.com>
2015-10-21 15:54:19 +0200

    Color picker core

--- app/color-picker/color-picker.js
+++ app/color-picker/color-picker.js
@@ -5,13 +5,47 @@
 
   // Define and register <color-picker> that uses Shadow DOM.
   var colorPickerProto = Object.create(HTMLElement.prototype);
-  var colorPickerTemplate = importDocument.querySelector('template');
+  var colorPickerTemplate = importDocument.querySelector('template').content;
 
     colorPickerProto.createdCallback = function () {
       var root = this.createShadowRoot();
-      root.appendChild(colorPickerTemplate.content);
-        //colorPickerTemplate.content.cloneNode(true);
+      root.appendChild(colorPickerTemplate);
+      //colorPickerTemplate.content.cloneNode(true);
     };
 
     document.registerElement('color-picker', { prototype: colorPickerProto });
+
+  var $enterButton = colorPickerTemplate.querySelector('#_cp_enter_button'),
+      $enterInput = colorPickerTemplate.querySelector('#_cp_enter_input'),
+      $selectedColorContainer = colorPickerTemplate.querySelector('#_cp_selected_color'),
+      $selectedHex = colorPickerTemplate.querySelector('#_cp_selected-hex'),
+      $selectedRgb = colorPickerTemplate.querySelector('#_cp_selected-rgb');
+
+  $enterButton.addEventListener('click', handleEnter);
+
+  function handleEnter(e) {
+    var colorHexText = $enterInput.value,
+        colorRGB;
+
+    if (colorHexText.length > 2) {
+      if (colorHexText.length < 6) {
+        colorHexText = colorHexText.substr(0, 3) + colorHexText.substr(0, 3);
+      }
+
+      colorRGB = hexToRgb(colorHexText);
+
+      $selectedColorContainer.style.background = $selectedHex.innerText = '#' + colorHexText;
+      $selectedRgb.innerText = `rgb(${colorRGB.r}, ${colorRGB.g}, ${colorRGB.b})`;
+    }
+  }
+
+  function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  }
+
 })();
